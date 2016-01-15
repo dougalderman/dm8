fc8bb66ea62b44e3581435304b3f53f17f916f39
Doug Alderman <agnadcp-d1@yahoo.com>
2016-01-14 17:37:49 -0700

    first commit

---
+++ models/user.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        name: 'Rick',
+        catchphrase: 'Wubba Dubba Lub Dub'
+    },
+    {
+        name: 'Morty',
+        catchphrase: 'Awwe, Geez, Rick'
+    }
+]
