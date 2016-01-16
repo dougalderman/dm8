cd7c50c1d34669e5313b1788e0963e6bbf74e219
Doug Alderman <agnadcp-d1@yahoo.com>
2016-01-15 17:33:57 -0700

    1/15/16 changes

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var usersCtrl = require('./controllers/users_controller');
 
+// var cors = require('cors');
+
 var app = express();
 
+app.use(session({
+    secret: 'sdfasf-eweras3232-asdfwe',
+    saveUnitialized: true,
+    resave: true
+}));
+
+// app.use(express.static(__dirname + '/public'));
+// app.use(cors());
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    console.log(req.session);
+    next();
+});
+
+app.post('/cart', function(req, res, next) {
+    if (!req.session.cart) {
+        req.session.cart = [];
+    }
+    req.session.cart.push(req.body);
+    res.json(req.session.cart);
+})
 
 app.get('/users', usersCtrl.index);
 app.get('/users/:id', usersCtrl.show);
